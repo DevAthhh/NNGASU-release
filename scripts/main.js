bd05502b389d25fe18d44b4c16e4bb71d1c0c7c1
BraunKc <152529764+BraunKc@users.noreply.github.com>
2024-05-10 22:01:30 +0300

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -30,3 +30,17 @@ function howToReset(action) {
         saveLastVisit();
     }
 }
+
+let slideIndex = 0;
+function slideShow() {
+    let i;
+    const slides = document.getElementsByClassName('slide');
+    for(i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(slideShow, 4000);
+}
+slideShow();
